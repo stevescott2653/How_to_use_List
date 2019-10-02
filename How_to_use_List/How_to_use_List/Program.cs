cc7c03953dbceef3bf735157158a9ca348ed0d55
Steven Scott <stevescott2653@gmail.com>
2019-10-02 16:16:07 -0400

    Add files via upload

---
+++ How_to_use_List/How_to_use_List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace How_to_use_List
+{
+    public class ListExample
+    {
+        static void Main(string[] args)
+        {
+            //Create a list of strings
+            var names = new List<string>();
+            names.Add("John Doe");
+            names.Add("Paul");
+            names.Add("Bob");
+            names.Add("Kevin");
+
+            // Iterate list element using foreach loop
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
